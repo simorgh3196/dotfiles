1b7951311cd96911fbc5d420ae859324d6b68be1
Tomoya Hayakawa <simorgh3196@TomoyanoMacBook-Pro.local>
2026-07-15 23:34:09 +0900

    feat: fishのシェル統合をasdf shimsからmise activateに変更
    手動のshims PATH追加処理と手動保守のasdf補完スクリプトを、
    mise公式推奨の `mise activate fish | source` に置き換え。
    mise は動的補完を内蔵するため独自の補完スクリプトは不要。

--- fish/config.fish
+++ fish/config.fish
@@ -123,21 +123,8 @@ set -gx EDITOR (which nvim)
 set -gx VISUAL $EDITOR
 set -gx SUDO_EDITOR $EDITOR
 
-# for asdf (version manager)
-# ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-# Do not use fish_add_path (added in Fish 3.2) because it
-# potentially changes the order of items in PATH
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
-# source (brew --prefix asdf)/libexec/asdf.fish
-source (brew --prefix asdf)/share/fish/vendor_completions.d/asdf.fish
+# for mise (version manager)
+mise activate fish | source
 
 # for homebrew at M1
 fish_add_path /opt/homebrew/bin
